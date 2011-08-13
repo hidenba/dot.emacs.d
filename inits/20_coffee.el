69103fe70ad598aab5ea7af0794142ce57b2d934
HIDEKUNI Kajita <hide.nba@gmail.com>
2011-08-13 19:28:24 +0900

    javascript関連のelを追加

---
+++ inits/20_coffee.el
@@ -0,0 +1,12 @@
+(setq load-path (cons "~/.emacs.d/elisp/coffee-mode" load-path))
+(require 'coffee-mode)
+
+(add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
+(add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))
+
+(defun coffee-custom ()
+  "coffee-mode-hook"
+ (set (make-local-variable 'tab-width) 2))
+
+(add-hook 'coffee-mode-hook
+  '(lambda() (coffee-custom)))
